8ad1599fe9e8157642290e0a639069ff0fd63f7e
TonyPythoneer <tonypythoneer@gmail.com>
2016-04-30 00:45:51 +0800

    Edit: I miss to upload ts-react-native-redux-starter-kit

---
+++ ts-react-native-redux-starter-kit/src/app/connect-args.ts
@@ -0,0 +1,14 @@
+//import componentApp from '../counter/counter.container';
+//import * as componentActions from '../counter/counter.actions';
+
+
+export function mapStateToProps(state) {
+    return //{ counter: state.counter };
+}
+
+
+export function mapDispatchToProps(dispatch) {
+    return {
+        //counterActions: bindActionCreators(counterActions, dispatch)
+    };
+}
---
+++ ts-react-native-redux-starter-kit/src/app/reducer.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+//import containerApp from '../container-app/container-app.reducer'
+
+
+export default <Redux.Reducer>combineReducers({
+});
